36a3f9ee05fcf98f871f13f4ddd61a4af125bc78
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-06-24 17:54:11 +1200

    refactoring GameDay

--- NetballApp/components/GameDay/GameDay.js
+++ NetballApp/components/GameDay/GameDay.js
@@ -1,18 +1,20 @@
 import { View, Text, Pressable, StyleSheet, TextInput } from "react-native";
 import { useState } from "react";
-import DateTimePicker from "@react-native-community/datetimepicker";
+import DatePicker from "./DatePicker";
 import InputSpinner from "react-native-input-spinner";
+import RoundsSelection from "./RoundsSelection";
+import { storeData } from "../Utils/LocalStorage";
 
 const GameDay = () => {
   const [team1, setTeam1] = useState("Team 1");
   const [team2, setTeam2] = useState("Team 2");
   const [selectedDate, setSelectedDate] = useState(new Date());
 
-  const [showDatePicker, setShowDatePicker] = useState(false);
-  const [value, setValue] = useState(5);
+  const [selectedRound, setSelectedRound] = useState(null);
+  const [timePerRound, setTimePerRound] = useState(10);
 
-  const handleValueChange = (value) => {
-    setValue(value);
+  const handleValueChange = (timePerRound) => {
+    setTimePerRound(timePerRound);
   };
 
   const handleDateChange = (event, date) => {
@@ -20,6 +22,19 @@ const GameDay = () => {
     if (date) {
       setSelectedDate(date);
     }
+  };
+
+  const savingGameInfo = async () => {
+    const newGameData = {
+      team1: team1,
+      team2: team2,
+      selectedDate: selectedDate.toString(),
+      rounds: selectedRound,
+      timePerRound: timePerRound,
+    };
+    // Save or process newGameData as needed
+    console.log(newGameData);
+    await storeData(newGameData);
   };
 
   return (
@@ -47,42 +62,16 @@ const GameDay = () => {
               placeholder="Team 2"
             />
           </View>
-          <View style={styles.groupingTextInput}>
-            <Text style={[styles.groupText, styles.fontSize]}>Date</Text>
-            <Pressable
-              style={styles.input}
-              onPress={() => setShowDatePicker(true)}
-            >
-              <Text>{selectedDate.toDateString()}</Text>
-            </Pressable>
-          </View>
-          {showDatePicker && (
-            <DateTimePicker
-              value={selectedDate}
-              mode="date"
-              display="default"
-              onChange={handleDateChange}
-              style={styles.dateTimePicker}
-            />
-          )}
+          <DatePicker
+            selectedDate={selectedDate}
+            handleDateChange={handleDateChange}
+          />
 
-          <View style={styles.roundGrouping}>
-            <Text style={[styles.roundText, styles.fontSize]}>Rounds</Text>
-            <View style={styles.roundButtonSpacing}>
-              <Pressable style={styles.roundButtons}>
-                <Text style={styles.fontSize}>1</Text>
-              </Pressable>
-              <Pressable style={styles.roundButtons}>
-                <Text style={styles.fontSize}>2</Text>
-              </Pressable>
-              <Pressable style={styles.roundButtons}>
-                <Text style={styles.fontSize}>3</Text>
-              </Pressable>
-              <Pressable style={styles.roundButtons}>
-                <Text style={styles.fontSize}>4</Text>
-              </Pressable>
-            </View>
-          </View>
+          <RoundsSelection
+            handleRoundSelection={setSelectedRound}
+            selectedRound={selectedRound}
+          />
+
           <View style={styles.groupingTextInput}>
             <Text style={[styles.groupText, styles.fontSize]}>
               Time (Per Round)
@@ -91,8 +80,10 @@ const GameDay = () => {
               <InputSpinner
                 max={60}
                 min={0}
-                step={5}
-                value={value}
+                step={1}
+                value={timePerRound}
+                buttonFontSize={34}
+                fontSize={24}
                 colorLeft="#219EBC"
                 colorRight="#219EBC"
                 colorPress="#8ECAE6"
@@ -101,12 +92,11 @@ const GameDay = () => {
               />
             </View>
           </View>
-
           <View style={styles.groupingActionButtons}>
             <Pressable disabled style={styles.actionButtons}>
               <Text style={styles.actionButtonText}>Store Game</Text>
             </Pressable>
-            <Pressable style={styles.actionButtons}>
+            <Pressable onPress={savingGameInfo} style={styles.actionButtons}>
               <Text style={styles.actionButtonText}>Play Game</Text>
             </Pressable>
           </View>
@@ -122,6 +112,7 @@ const styles = StyleSheet.create({
   headingFontSize: {
     color: "#023047",
     fontSize: 24,
+    marginTop: 40,
   },
   fontSize: {
     color: "#023047",
@@ -161,32 +152,6 @@ const styles = StyleSheet.create({
   },
   timeInput: {
     paddingHorizontal: 10,
-  },
-  roundGrouping: {
-    width: "100%",
-  },
-  roundText: {
-    paddingHorizontal: 25,
-    paddingBottom: 2,
-  },
-  roundButtonSpacing: {
-    width: "100%",
-    paddingHorizontal: 25,
-    flexWrap: "wrap",
-    flexDirection: "row",
-    columnGap: 10,
-    rowGap: 10,
-    justifyContent: "space-between",
-    alignSelf: "center",
-  },
-  roundButtons: {
-    justifyContent: "space-between",
-    paddingVertical: 15,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    borderColor: "#E6E6E6",
-    borderWidth: 2,
-    alignItems: "center",
   },
   groupingActionButtons: {
     width: "90%",
