0b6c7ff2e7ab44093192cb7c8da8e967f0e8763a
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-06-28 16:26:02 +1200

    adding rounds to check when the game is over

--- NetballApp/components/Scoring/Scoring.js
+++ NetballApp/components/Scoring/Scoring.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 import CountDown from "react-native-countdown-component";
 import Counter from "./Counter";
+import CounterBoard from "./CounterBoard";
 
 const ScoringPage = ({ route, navigate }) => {
   const { newGameData } = route.params;
+  const totalRounds = parseInt(newGameData.rounds);
   const timer = parseInt(newGameData.timePerRound);
+  const [currentRound, setCurrentRound] = useState(1);
   const [isTimerRunning, setIsTimerRunning] = useState(false);
 
   const [team1ScoreGS, setTeam1ScoreGS] = useState(0);
@@ -55,49 +58,93 @@ const ScoringPage = ({ route, navigate }) => {
     setIsTimerRunning(!isTimerRunning);
   };
 
+  const handleTimerFinish = () => {
+    if (currentRound < totalRounds) {
+      setCurrentRound(currentRound + 1);
+      setIsTimerRunning(false);
+    } else {
+      alert("All rounds completed!");
+    }
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Round: {newGameData.rounds}</Text>
-      <Text>{newGameData.team1}</Text>
-      <Text>{newGameData.team2}</Text>
-      <Text>{timer}</Text>
-      <View>
+    <View style={styles.container}>
+      <Text style={styles.roundText}>Round: {currentRound}</Text>
+      <Text style={styles.teamText}>{newGameData.team1}</Text>
+      <Text>VS</Text>
+      <Text style={styles.teamText}>{newGameData.team2}</Text>
+      <View style={styles.countdownContainer}>
         <CountDown
           until={60 * timer}
           size={30}
-          onFinish={() => alert("Finished")}
+          onFinish={handleTimerFinish}
           digitStyle={{ backgroundColor: "#FFF" }}
           digitTxtStyle={{ color: "#1CC625" }}
           timeToShow={["M", "S"]}
           timeLabels={{ m: "MM", s: "SS" }}
           running={isTimerRunning}
+          onPress={handleTimerToggle}
         />
       </View>
       <Pressable onPress={handleTimerToggle}>
-        <Text>{isTimerRunning ? "Stop" : "Start"} Timer</Text>
+        <Text style={styles.timerButton}>
+          {isTimerRunning ? "Stop" : "Start"} Timer
+        </Text>
       </Pressable>
-
-      <View>
-        <Text style={{ fontSize: 18, marginBottom: 20 }}>Team 1</Text>
-        <Counter
+      <View style={styles.countersContainer}>
+        <CounterBoard
+          onDecrement={() => decrementTeamScore(1)}
           value={team1Score}
+        />
+        <Text>VS</Text>
+        <CounterBoard
+          label={"team 2"}
+          onDecrement={() => decrementTeamScore(2)}
+          value={team2Score}
+        />
+      </View>
+      <View style={styles.countersContainer}>
+        <Counter
           onIncrementGS={() => incrementTeamScoreGS(1)}
           onIncrementGA={() => incrementTeamScoreGA(1)}
-          onDecrement={() => decrementTeamScore(1)}
         />
-
-        <View style={{ marginTop: 20 }}>
-          <Text style={{ fontSize: 18, marginBottom: 20 }}>Team 2</Text>
-          <Counter
-            value={team2Score}
-            onIncrementGS={() => incrementTeamScoreGS(2)}
-            onIncrementGA={() => incrementTeamScoreGA(2)}
-            onDecrement={() => decrementTeamScore(2)}
-          />
-        </View>
+        <Counter
+          onIncrementGS={() => incrementTeamScoreGS(2)}
+          onIncrementGA={() => incrementTeamScoreGA(2)}
+        />
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  roundText: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  teamText: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  timerText: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  countdownContainer: {
+    marginBottom: 20,
+  },
+  timerButton: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  countersContainer: {
+    flexDirection: "row",
+  },
+});
 
 export default ScoringPage;
